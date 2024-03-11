d3ad2f645143bbbcb184e0800260b59f49d8295d
joao marcelo neres vieira <john.collins.vieira@gmail.com>
2024-03-10 21:13:18 -0400

    added data to the database

--- lib/seed.py
+++ lib/seed.py
@@ -11,14 +11,6 @@ def seed_database():
     Book.create_table()
 
     # Create seed data
-    payroll = Genre.create("Payroll")
-    human_resources = Genre.create("Human Resources")
-    Book.create("Amir", "Accountant", payroll.id)
-    Book.create("Bola", "Manager", payroll.id)
-    Book.create("Charlie", "Manager", human_resources.id)
-    Book.create("Dani", "Benefits Coordinator", human_resources.id)
-    Book.create("Hao", "New Hires Coordinator", human_resources.id)
-
 
 seed_database()
 print("Seeded database")
